eb002778c58dc7700ffdcb66b435211b56b762f2
Diego Novaes <d.novaes824@gmail.com>
2022-04-12 09:51:41 -0300

    Primeiras telas criadas

---
+++ frontend/src/pages/FirstScreen/index.tsx
@@ -0,0 +1,7 @@
+export default function FirstSecreen () {
+    return (
+        <>
+            
+        </>
+    );
+}
--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,12 +1,18 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 
-import Home from "./pages/Home";
+import HomeScreen from "./pages/HomeScreen";
+import LoginSecreen from "./pages/LoginScreen";
+import FirstSecreen from "./pages/FirstScreen";
+import RegistrationSecreen from "./pages/RegistrationScreen";
 
 export default function AppRoutes() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home/>}/>
+                <Route path="/" element={<FirstSecreen/>}/>
+                <Route path="/home_screen" element={<HomeScreen/>}/>
+                <Route path="/login_secreen" element={<LoginSecreen/>}/>
+                <Route path="/registration_secreen" element={<RegistrationSecreen/>}/>
             </Routes>
         </BrowserRouter>
     );
